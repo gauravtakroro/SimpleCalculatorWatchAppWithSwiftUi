5ff88ef64cab54b5830bf403e98af6dee645640b
Gaurav Tak <gaurav.tak@roro.io>
2023-08-18 00:05:38 +0530

    CalcButton Implemented

---
+++ SimpleCalc Watch App/CalcUiViews/CalcButton.swift
@@ -0,0 +1,33 @@
+//
+//  CalcButton.swift
+//  SimpleCalc Watch App
+//
+//  Created by Gaurav Tak on 18/08/23.
+//
+
+import Foundation
+
+enum CalcButton: String {
+    case one = "1"
+    case two = "2"
+    case three = "3"
+    case four = "4"
+    case five = "5"
+    case six = "6"
+    case seven = "7"
+    case eight = "8"
+    case nine = "9"
+    case zero = "0"
+    case add = "+"
+    case subtract = "-"
+    case divide = "÷"
+    case mutliply = "x"
+    case equal = "="
+    case clear = "AC"
+    case decimal = "."
+}
+
+enum ArithmeticOperation {
+    case add, subtract, multiply, divide, none
+}
+
