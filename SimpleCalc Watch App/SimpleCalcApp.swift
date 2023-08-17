72afb266d2e1a20a5fe4451e8bc0d23579353dd4
Gaurav Tak <gaurav.tak@roro.io>
2023-08-17 23:50:02 +0530

    Initial Commit

---
+++ SimpleCalc Watch App/SimpleCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCalcApp.swift
+//  SimpleCalc Watch App
+//
+//  Created by Roro Solutions LLP on 17/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCalc_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
